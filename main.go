ce8b8d6131a44f069b9f9402121ff25f1369d508
hajji ismail <hajji.ismail.851@gmail.com>
2024-11-02 17:59:08 +0100

    adding the G-A-B-I

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"groupie-tracker/server"
@@ -8,5 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", server.Home)
+	http.HandleFunc("/static/" , server.ServStatic)
+	fmt.Println("the server is running on the port 5050 -->> http://localhost:5050")
 	http.ListenAndServe(":5050", nil)
 }
--- server/server.go
+++ server/server.go
@@ -4,12 +4,16 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"groupie-tracker/fetching"
+	"groupie-tracker/models"
 )
 
 type Parse struct {
-	Index     *template.Template
+	Index *template.Template
+
 	ErrorTemp *template.Template
 }
 
@@ -22,6 +26,7 @@ func init() {
 		return
 	}
 	parsing.Index = Index
+
 	ErrorTemp, err := template.ParseFiles("template/error.html")
 	if err != nil {
 		log.Fatal("I can't parse the error.html file")
@@ -48,4 +53,64 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		parsing.ErrorTemp.Execute(w, "Internal Server Error")
 	}
+}
+
+func GetArtistByID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		parsing.ErrorTemp.Execute(w, "Method Not Allowed")
+		return
+	}
+
+	// Extract the artist ID from the URL
+	artistIDStr := r.URL.Path[len("/artist/"):] // Assuming the URL is like /artist/{id}
+	artistID, err := strconv.Atoi(artistIDStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		parsing.ErrorTemp.Execute(w, "Invalid Artist ID")
+		return
+	}
+
+	// Fetch the artist data using the artist ID
+	var artist *models.Artist
+	artists := fetching.Artists // Assuming this is of type *[]models.Artist
+
+	// Dereference the pointer to access the slice
+	for _, a := range *artists {
+		if a.Id == artistID { // Assuming each artist has an ID field
+			artist = &a
+			break
+		}
+	}
+
+	if artist == nil {
+		w.WriteHeader(http.StatusNotFound)
+		parsing.ErrorTemp.Execute(w, "Artist Not Found")
+		return
+	}
+
+	// Render the artist data using a template (you may need to create this template)
+	err = parsing.Index.Execute(w, artist) // Use a different template if necessary
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		parsing.ErrorTemp.Execute(w, "Internal Server Error")
+	}
+}
+
+func ServStatic(w http.ResponseWriter, r *http.Request) {
+	// Check if the request is for the CSS directory itself
+	if r.URL.Path == "/css/" || r.URL.Path == "/css" {
+		w.WriteHeader(http.StatusNotFound)
+		parsing.ErrorTemp.Execute(w, "NOT FOUND")
+		return
+	}
+	_, err := os.Stat(r.URL.Path[1:])
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		parsing.ErrorTemp.Execute(w, "NOT FOUND")
+		return
+	}
+
+	// Serve the CSS file if it's a valid request
+	http.StripPrefix("/static/", http.FileServer(http.Dir("static"))).ServeHTTP(w, r)
 }
