81f44dcd15ddd131e729a528e32c30ed68781a50
hajji-ismail <hajji.ismail.851@gmail.com>
2024-11-03 11:14:01 +0100

    fetching

--- fetching/locations.go
+++ fetching/locations.go
@@ -1,45 +1,51 @@
 package fetching
 
 import (
-    "encoding/json"
-    "fmt"
-    "io"
-    "log"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 
-    "groupie-tracker/models"
+	"groupie-tracker/models"
 )
 
-var local []models.Location
+var local *[]models.Location
 
 func init() {
-    var err error
-    local, err = fetchlocal("https://groupietrackers.herokuapp.com/api/locations")
-    if err != nil {
-        log.Fatal(err)
-    }
+	var err error
+	local, err = fetchlocal("https://groupietrackers.herokuapp.com/api/locations")
+	fmt.Println(local)
+	
+	if err != nil {
+
+		log.Fatal(err)
+	}
+
 }
 
-func fetchlocal(url string) ([]models.Location, error) {
-    response, err := http.Get(url)
-    if err != nil {
-        return nil, err
-    }
-    defer response.Body.Close()
+func fetchlocal(url string) (*[]models.Location, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-    if response.StatusCode != http.StatusOK {
-        return nil, fmt.Errorf("failed to fetch data: status code %d", response.StatusCode)
-    }
+	if resp.StatusCode != http.StatusOK {
+		log.Println(resp.Status)
+		return nil, err
+	}
+	local, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
 
-    body, err := io.ReadAll(response.Body)
-    if err != nil {
-        return nil, err
-    }
-
-    var loc []models.Location
-    err = json.Unmarshal(body, &loc)
-    if err != nil {
-        return nil, err
-    }
-    return loc, nil
+	var loc []models.Location
+	err = json.Unmarshal(local, &loc)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return &loc, nil
 }
